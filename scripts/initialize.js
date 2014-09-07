039d1e3c31523d28146bfd2079deb2a9d08eaa06
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-09-07 17:27:13 -0300

    Add undo-redo functionality and refactor some code
    Change-Id: I40cc43759938592363ea98de6589f868cbd4c76a

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,5 +1,7 @@
+// Sketchpad as a global variable, so it becomes browser accessible
+var sketchpad = null;
 $(document).ready(function() {
-  var sketchpad = Sketchpad({
+  sketchpad = new Sketchpad({
     element: '#sketchpad',
     width: window.innerWidth,
     height: window.innerHeight,
--- scripts/sketchpad.js
+++ scripts/sketchpad.js
@@ -9,38 +9,63 @@ function Sketchpad(config) {
   this.element = config.element;
 
   // Width can be defined on the HTML or programatically
-  var width = config.width || $(element).attr('data-width') || 0;
-  var height = config.height || $(element).attr('data-height') || 0;
+  var width = config.width || $(this.element).attr('data-width') || 0;
+  var height = config.height || $(this.element).attr('data-height') || 0;
 
-  this.color = config.color || $(element).attr('data-color') || '#000000';
-  this.penSize = config.penSize || $(element).attr('data-penSize') || 5;
+  // Pen attributes
+  this.color = config.color || $(this.element).attr('data-color') || '#000000';
+  this.penSize = config.penSize || $(this.element).attr('data-penSize') || 5;
+
+  // Stroke control variables
+  this._strokes = [];
+  this._currentStroke = {
+    color: null,
+    size: null,
+    lines: [],
+  }
+
+  // Undo History
+  this._undoHistory = [];
 
   //
   // Private API
   //
 
-  var cursorPosition = function(event) {
+  this._cursorPosition = function(event) {
     return {
       x: event.pageX - this.canvas.offsetLeft,
       y: event.pageY - this.canvas.offsetTop,
     };
   };
 
-  var cursorMove = function(event) {
-    var currentPosition = cursorPosition(event);
+  this._cursorMove = function(event) {
+    var currentPosition = this._cursorPosition(event);
 
-    draw(this._lastPosition, currentPosition, this.color, this.penSize);
+    this._draw(this._lastPosition, currentPosition, this.color, this.penSize);
+    this._currentStroke.lines.push({
+      start: $.extend(true, {}, this._lastPosition),
+      end: $.extend(true, {}, currentPosition),
+    });
 
     this._lastPosition = currentPosition;
   };
 
-  var draw = function(start, end, color, size) {
+  this._draw = function(start, end, color, size) {
+    this._stroke(start, end, color, size, 'source-over');
+  };
+
+  this._erase = function(start, end, color, size) {
+    this._stroke(start, end, color, size, 'destination-out');
+  };
+
+  this._stroke = function(start, end, color, size, compositeOperation) {
     this.context.save();
 
     this.context.lineJoin = 'round';
     this.context.lineCap = 'round';
     this.context.strokeStyle = color;
     this.context.lineWidth = size;
+    this.context.globalCompositeOperation = compositeOperation;
 
     this.context.beginPath();
     this.context.moveTo(start.x, start.y);
@@ -63,18 +88,63 @@ function Sketchpad(config) {
     this.context = this.canvas.getContext('2d');
 
     // Setup event listeners
-    var view = this;
-    var callback = cursorMove.bind(this);
+    var sketching = false;
+    var callback = this._cursorMove.bind(this);
     this.canvas.addEventListener('mousedown', function(event) {
-      view._lastPosition = cursorPosition(event);
-      view.canvas.addEventListener('mousemove', callback);
-    });
+      this._lastPosition = this._cursorPosition(event);
+      this._currentStroke.color = this.color;
+      this._currentStroke.size = this.penSize;
+      this._currentStroke.lines = [];
+      sketching = true;
+      this.canvas.addEventListener('mousemove', callback);
+    }.bind(this));
     this.canvas.addEventListener('mouseout', function(event) {
-      view.canvas.removeEventListener('mousemove', callback);
-    });
+      if (sketching) {
+        this._strokes.push($.extend(true, {}, this._currentStroke));
+        sketching = false;
+      }
+      this.canvas.removeEventListener('mousemove', callback);
+    }.bind(this));
     this.canvas.addEventListener('mouseup', function(event) {
-      view.canvas.removeEventListener('mousemove', callback);
-    });
+      if (sketching) {
+        this._strokes.push($.extend(true, {}, this._currentStroke));
+        sketching = false;
+      }
+      this.canvas.removeEventListener('mousemove', callback);
+    }.bind(this));
+  };
+
+  this.drawStroke = function(stroke) {
+    for (var j = 0; j < stroke.lines.length; j++) {
+      var line = stroke.lines[j];
+      this._draw(line.start, line.end, stroke.color, stroke.size);
+    }
+  };
+
+  this.redraw = function(strokes) {
+    for (var i = 0; i < strokes.length; i++) {
+      this.drawStroke(strokes[i]);
+    }
+  };
+
+  //
+  // Public Functionalities
+  //
+
+  this.clear = function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  };
+
+  this.undo = function() {
+    this.clear();
+    this._undoHistory.push(this._strokes.pop());
+    this.redraw(this._strokes);
+  };
+
+  this.redo = function() {
+    var stroke = this._undoHistory.pop();
+    this._strokes.push(stroke);
+    this.drawStroke(stroke);
   };
 
   this.reset();
